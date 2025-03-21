8e5fcde8df77e6de0e35e94df1724417972f168c
Kauan Curbani <crbnii2020@gmail.com>
2025-03-21 15:45:04 -0300

    redirect page in client side

--- src/app/r/[id]/page.tsx
+++ src/app/r/[id]/page.tsx
@@ -1,20 +1,25 @@
+"use client";
 import { api } from "@/lib/api";
 import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 type tParams = Promise<{ id: string }>;
 
-export default async function Page({ params }: { params: tParams }) {
-  const { id } = await params;
-
-  
-  const [ response ] = await Promise.all([
-    api.get(`/api/short-url/${id}`),
-    api.get(`/api/short-url/consume/${id}`)
-  ]) 
-  
-  if (response.data.url) {
-    redirect(response.data.url);
-  }
+export default function Page({ params }: { params: tParams }) {
+  useEffect(() => {
+    (async () => {
+      const { id } = await params;
+      Promise.all([
+        api.get(`/api/short-url/${id}`),
+        api.get(`/api/short-url/consume/${id}`),
+      ]).then((res) => {
+        const [response] = res;
+        if (response.data.url) {
+          redirect(response.data.url);
+        }
+      });
+    })();
+  }, []);
 
   return <div></div>;
 }
