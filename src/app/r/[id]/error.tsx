2bfa1e8004fee5df6bf208416b53a19da6ea513d
Kauan Curbani <crbnii2020@gmail.com>
2025-03-21 15:07:30 -0300

    consume url

---
+++ src/app/r/[id]/error.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+
+export default function Error () {
+ 
+  return <div className="flex min-h-dvh justify-center items-center flex-col">
+    <span>
+      Occurred an error, sorry ☹️
+    </span>
+  </div>
+}
--- src/app/r/[id]/page.tsx
+++ src/app/r/[id]/page.tsx
@@ -5,8 +5,13 @@ type tParams = Promise<{ id: string }>;
 
 export default async function Page({ params }: { params: tParams }) {
   const { id } = await params;
-  const response = await api.get(`/api/short-url/${id}`);
 
+  
+  const [ response ] = await Promise.all([
+    api.get(`/api/short-url/${id}`),
+    api.get(`/api/short-url/${id}/consume`)
+  ]) 
+  
   if (response.data.url) {
     redirect(response.data.url);
   }
